6a3679113897abcd2b4e26df26048f04be04b5d8
RazelRaz <razel16dcm@gmail.com>
2025-11-17 19:12:16 +0600

    CSV Data Uploader file ready

---
+++ rz-csv-data-uploader.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Plugin Name:       RZ CSV Data Uploader
+ * Plugin URI:        https://example.com
+ * Description:       A plugin to create and practice shortcode widget
+ * Version:           1.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Razel Ahmed
+ * Author URI:        https://author.example.com/
+ * License:           GPL v2 or later
+ */
+
+defined('ABSPATH') || exit;
