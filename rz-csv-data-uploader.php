afbf2fe3fbf4d415e0ac97ed9fefeeffa4adff99
RazelRaz <razel16dcm@gmail.com>
2025-11-17 20:01:51 +0600

    class called and folder creation

--- rz-csv-data-uploader.php
+++ rz-csv-data-uploader.php
@@ -2,8 +2,8 @@
 /*
  * Plugin Name:       RZ CSV Data Uploader
  * Plugin URI:        https://example.com
- * Description:       A plugin to create and practice shortcode widget
- * Version:           1.1
+ * Description:       This plugin will upload CSV data to DataBase Table
+ * Version:           1.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Razel Ahmed
@@ -12,3 +12,19 @@
  */
 
 defined('ABSPATH') || exit;
+
+// Include files
+require_once( plugin_dir_path(__FILE__) . "include/cdu_form.php" );
+
+class Rz_csv_data_uploader {
+    function __construct(){
+        add_shortcode( "csv-data-uploader", [ $this, "rz_display_uploader_form"] );
+    }
+
+    function rz_display_uploader_form() {
+        return "<h3>CSV DATA UPLOADER</h3>";
+    }
+
+}
+
+new Rz_csv_data_uploader();
